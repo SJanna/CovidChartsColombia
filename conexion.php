325c2b0aa80a086a1ed4110eca4549e7b008a13a
Shalem Janna <70728090+SJanna@users.noreply.github.com>
2022-04-14 22:54:38 -0500

    Azure Connection
    Se cambio de una base de datos local a una base de datos en la nube con Azure

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "SHALEM_JANNA"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"ColombiaCovidData","UID"=>"sa", "PWD"=>"123456789");
+$serverName = "tcp:graficascovidserver.database.windows.net,1433"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"graficasCovid","UID"=>"shalem", "PWD"=>"A1b2c3008205135");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 ?>

7baf43d323efd1234f03d9a8c76915d78840a3ac
Shalem Janna <70728090+SJanna@users.noreply.github.com>
2022-04-14 23:05:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,10 @@
 
 <body>   
     <?php
-    include('conexion.php');
+    $serverName = "tcp:graficascovidserver.database.windows.net,1433"; //serverName\instanceName
+    $connectionInfo = array( "Database"=>"graficasCovid","UID"=>"shalem", "PWD"=>"A1b2c3008205135");
+    $conn = sqlsrv_connect( $serverName, $connectionInfo);
     ?>
-    
     <p></p>
     <div class="row">
         <div class="bigColumn">
